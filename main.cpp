fe858751996bc852144d603e5d3ff84d9636fb79
Clarisse <clarisse.fleu@gmail.com>
2019-03-06 16:57:46 +0100

    Methode rotation() récursive

--- main.cpp
+++ main.cpp
@@ -12,29 +12,42 @@
 
 using namespace std;
 
-void rotationPiece(Piece piece)
-{
-
-}
+void rotation(const Piece& piece, int rotNum, Pieces& rotations);
 
 int main() {
-   
-   
-   //bonjour
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   //aurevoir
+    Pieces pieces;
+    cout << PIECES.size() << " pieces." << endl;
+    
+    for (int i = 0; i < PIECES.size(); ++i) {
+        pieces.push_back(PIECES.at(i));
+        rotation(PIECES.at(i), 0, pieces);
+    }
+    cout << pieces.size() << " rotations en tout." << endl;
     
     return 0;
+}
+
+bool placerPiece(const Piece& piece) {
+    
+    return true;
+}
+
+void rotation(const Piece& piece, int rotNum, Pieces& rotations) {
+    
+    if(rotNum == 3) {
+        return;
+    } else {
+        Piece r;
+        
+        AttachementType temp = piece.at(rotNum);
+        
+        for (int i = 0; i < 3; ++i) {
+            r.at(i) = piece.at(i+1);
+        }
+        r.at(3) = temp;
+        
+        rotations.push_back(r);
+        
+        rotation(r, rotNum+1, rotations);
+    }
 }
