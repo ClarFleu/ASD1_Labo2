0760c661393196b86d7055b86ffe3f3f2678ea38
Clarisse <clarisse.fleu@gmail.com>
2019-03-18 14:02:02 +0100

    Un peu mieux mais quand meme faux...

--- clarisseMain.hpp
+++ clarisseMain.hpp
@@ -46,8 +46,13 @@ bool operator==(const Piece& lhs, const Piece& rhs);
 void rotation(const Piece& piece, int rotNum, Pieces& rotations);
 
 void casseTete(Pieces& listePieces, Pieces& plan);
+void casseTeteX(const Pieces& listePieces, unsigned indicePiece, Pieces& plan);
 
 AttachementType visAVis(size_t positionPiece, size_t positionImage);
+
+bool memePiece(const Piece& lhs, const Piece& rhs);
+
+bool piecePeutEtrePlacee(const Piece& piece, size_t position, const Pieces& plan);
 
 /********
  * Main *
@@ -66,6 +71,7 @@ int main() {
     cout << pieces.size() << " rotations en tout." << endl;
     */
    
+   //casseTeteX(pieces, 0, plan);
    casseTete(pieces, plan);
    //placerPiece(solution, 0, 0);
    
@@ -82,6 +88,81 @@ int main() {
 /***************************
  * Définitions de methodes *
  ***************************/
+void casseTeteX(const Pieces& listePieces, unsigned indicePiece, Pieces& plan) {
+   if (listePieces.size() <= indicePiece) {
+      bool sol = true;
+      for (int i = 0; i < plan.size(); ++i) {
+         if (plan.at(i)==PIECE_VIDE) {
+            sol = false;
+            break;
+         }
+      }
+      if (sol) {
+         ++nbSolutions;
+         afficherSolution(plan);
+      }
+      return;
+   }
+   
+   Piece piece = listePieces.at(indicePiece);
+   
+   Pieces rotationsP;
+   rotationsP.push_back(piece);
+   rotation(piece, 0, rotationsP);
+   
+   // on parcourt toutes les rotations de la pieces
+   for (size_t rot = 0; rot < rotationsP.size(); ++rot) {
+      
+      int positionVide = -1;
+      for (int i = 0; i < plan.size(); ++i) {
+         if (plan.at(i) == PIECE_VIDE) {
+            positionVide = i;
+            break;
+         }
+      }
+      if (positionVide < 0) {
+         return;
+      }
+      
+      bool piecesPlaceable = true;
+      
+      //piecesPlaceable = imageComplete(rotationsP.at(rot).at(i), visAVis(positionPiece, i));
+      if (positionVide % 3 >= 1 && !imageComplete(plan.at(positionVide-1).at(1), piece.at(3))) {
+         piecesPlaceable = false;
+      }
+      if (positionVide % 3 <= 1 && !imageComplete(plan.at(positionVide+1).at(3), piece.at(1))) {
+         piecesPlaceable = false;
+      }
+      if (positionVide > 2 && !imageComplete(plan.at(positionVide-3).at(0), piece.at(0))) {
+         piecesPlaceable = false;
+      }
+      if (positionVide < 6 && !imageComplete(plan.at(positionVide+3).at(2), piece.at(2))) {
+         piecesPlaceable = false;
+      }
+      
+      piecesPlaceable = true;
+      
+      // On verifie si la piece est déjà placée
+      for (int i = 0; i < plan.size(); ++i) {
+         if (plan.at(i) == piece) {
+            piecesPlaceable = false;
+            break;
+         }
+      }
+      
+      // Si elle peut être placée
+      if (piecesPlaceable) {
+         // On la met à la bonne position sur le plan
+         plan.at(positionVide) = rotationsP.at(rot);
+         
+         // On appelle la récursive afin de continuer
+         casseTeteX(listePieces, indicePiece+1, plan);
+         
+         plan.at(positionVide) = PIECE_VIDE;
+      }
+   }
+}
+
 void casseTete(Pieces& listePieces, Pieces& plan) {
    if (listePieces.size() == 0) {
       bool sol = true;
@@ -101,44 +182,80 @@ void casseTete(Pieces& listePieces, Pieces& plan) {
    
    for (size_t pieceAPlacer = 0; pieceAPlacer < listePieces.size(); ++pieceAPlacer) {
       
-      size_t positionVide = -1;
-      for (size_t i = 0; i < plan.size(); ++i) {
-         if (plan.at(i) == PIECE_VIDE) {
-            positionVide = i;
-            break;
-         }
-      }
       
       Piece piece = listePieces.at(pieceAPlacer);
       
       Pieces rotationsP;
       rotationsP.push_back(piece);
       rotation(piece, 0, rotationsP);
-      bool piecesPlaceable = true;
       
       // on parcourt toutes les rotations de la pieces
       for (size_t rot = 0; rot < rotationsP.size(); ++rot) {
-         // on regarde si la rotation peut être placée
-         for (int i = 0; i < 4; ++i) {
-            //piecesPlaceable = imageComplete(rotationsP.at(rot).at(i), visAVis(positionPiece, i));
-            
-            if (!imageComplete(rotationsP.at(rot).at(i), visAVis(positionVide, i)) ) {
-               piecesPlaceable = false;
+         
+         int positionVide = -1;
+         for (int i = 0; i < plan.size(); ++i) {
+            if (plan.at(i) == PIECE_VIDE) {
+               positionVide = i;
+               break;
             }
-            //piecesPlaceable = true;
          }
+         if (positionVide < 0) {
+            return;
+         }
+         
+         bool piecesPlaceable = piecePeutEtrePlacee(piece, positionVide, plan);
+         
+         // On verifie si la piece est déjà placée
+         for (int i = 0; i < plan.size(); ++i) {
+            if (memePiece(plan.at(i), piece)) {
+               piecesPlaceable = false;
+               break;
+            }
+         }
+         
          // Si elle peut être placée
-         if (piecesPlaceable && positionVide < plan.size()) {
+         if (piecesPlaceable) {
             // On la met à la bonne position sur le plan
             plan.at(positionVide) = rotationsP.at(rot);
+            
+            // On l'enlève des pieces à placer
+            Piece tmp = listePieces.at(pieceAPlacer);
             listePieces.erase(listePieces.begin()+pieceAPlacer);
+            
             // On appelle la récursive afin de continuer
             casseTete(listePieces, plan);
-            listePieces.push_back(piece);
-            plan.at(pieceAPlacer) = PIECE_VIDE;
+            
+            listePieces.push_back(tmp);
+            plan.at(positionVide) = PIECE_VIDE;
          }
       }
    }
+}
+
+bool piecePeutEtrePlacee(const Piece& piece, size_t position, const Pieces& plan) {
+   bool rep = true;
+   if (position % 3 >= 1 && !imageComplete(plan.at(position-1).at(1), piece.at(3))) {
+      rep = false;
+   } else {
+      rep = true;
+   }
+   if (position % 3 <= 1 && !imageComplete(plan.at(position+1).at(3), piece.at(1))) {
+      rep = false;
+   } else {
+      rep = true;
+   }
+   if (position > 2 && !imageComplete(plan.at(position-3).at(0), piece.at(0))) {
+      rep = false;
+   } else {
+      rep = true;
+   }
+   if (position < 6 && !imageComplete(plan.at(position+3).at(2), piece.at(2))) {
+      rep = false;
+   } else {
+      rep = true;
+   }
+   
+   return rep;
 }
 
 void afficherSolution(const Pieces& sol) {
@@ -169,6 +286,18 @@ void rotation(const Piece& piece, int rotNum, Pieces& rotations) {
       
       rotation(r, rotNum + 1, rotations);
    }
+}
+
+bool memePiece(const Piece& lhs, const Piece& rhs) {
+   Pieces p;
+   p.push_back(lhs);
+   rotation(lhs, 0, p);
+   for (int i = 0; i < p.size(); ++i) {
+      if (p.at(i) == rhs) {
+         return true;
+      }
+   }
+   return false;
 }
 
 bool operator==(const Piece& lhs, const Piece& rhs) {
