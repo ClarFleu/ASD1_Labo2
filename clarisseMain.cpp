88ab79412daf100546eb67fae9ae8beb068ed418
Clarisse Fleurimont <clarisse.fleu@gmail.com>
2019-03-18 09:20:42 +0100

    Delete clarisseMain.cpp

--- clarisseMain.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  clarisseMain.cpp
-//  Labo2
-//
-//  Created by Clarisse Fleurimont on 18.03.19.
-//  Copyright © 2019 Clarisse Fleurimont. All rights reserved.
-//
-
-#include "clarisseMain.hpp"
